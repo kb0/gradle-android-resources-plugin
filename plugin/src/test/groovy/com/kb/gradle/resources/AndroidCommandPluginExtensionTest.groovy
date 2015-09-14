f23ebf0804dd50594fd3f1ba2ef43d55173fcd9b
kb <kb@na.ru>
2015-09-14 17:17:30 +0300

    update tests

--- plugin/src/test/groovy/com/kb/gradle/resources/AndroidCommandPluginExtensionTest.groovy
+++ plugin/src/test/groovy/com/kb/gradle/resources/AndroidCommandPluginExtensionTest.groovy
@@ -1,18 +1,39 @@
 package com.kb.gradle.resources
 
+import org.gradle.api.Project
+import org.gradle.api.tasks.StopExecutionException
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
 
-class AndroidResourcesPluginExtensionTest extends GroovyTestCase {
-    void testDefaultAdbPath() {
-        def extension = createExtension()
-        assert true
-    }
-
-    private static AndroidResourcesPluginExtension createExtension() {
-        def projectDir = new File('..')
-        def project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+class AndroidResourcesPluginExtensionTest {
+    @Test
+    public void canCreateExtension() {
+        // Project project = ProjectBuilder.builder().build()
         def extension = new AndroidResourcesPluginExtension()
-        extension
+        assert (extension instanceof AndroidResourcesPluginExtension)
     }
+
+    @Test
+    public void canAddBuildDrawablesTaskToProject() {
+        Project project = ProjectBuilder.builder().build()
+
+        def task = project.task('buildDrawables', type: BuildDrawablesTask)
+        assertTrue(task instanceof BuildDrawablesTask)
+    }
+
+    @Test
+    public void cannotApplyPluginToProjectWithoutAndroid() {
+        Project project = ProjectBuilder.builder().build()
+
+        try {
+            project.pluginManager.apply('android-resources')
+            assert false
+        } catch (StopExecutionException ignored) {
+            assert true
+        }
+        //assertTrue(task instanceof BuildDrawablesTask)
+    }
+
+
 }
 
